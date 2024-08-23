32c7f2334d4934d17df76defed5d5ad9bb997840
henkkapoika <henriknerg@hotmail.com>
2024-08-23 16:52:14 +0300

    tutorial1

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Tämän hetkinen tutoriaali: Angular Crash Course 2024 (for Beginners)
+
+https://www.youtube.com/watch?v=f7BJFTEbc10&t=295s
+
+Henrik Nerg
---
+++ tutorial/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  constructor(
+    private productsService: ProductsService
+  ) {}
+}
